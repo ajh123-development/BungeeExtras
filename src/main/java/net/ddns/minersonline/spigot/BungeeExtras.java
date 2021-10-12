528ecf2c2b7cd14ed454d83a0e0ab95dbc8b4d24
ajh123 <samrosoft36@gmail.com>
2021-10-12 20:31:00 +0100

    Temporary fix for version number

--- src/main/java/net/ddns/minersonline/spigot/BungeeExtras.java
+++ src/main/java/net/ddns/minersonline/spigot/BungeeExtras.java
@@ -1,6 +1,9 @@
 package net.ddns.minersonline.spigot;
 import net.ddns.minersonline.CommonExtras;
+import net.ddns.minersonline.spigot.commands.ServerInfoCommand;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Objects;
 
 public class BungeeExtras extends JavaPlugin implements CommonExtras {
     public static BungeeExtras plugin = null;
@@ -9,6 +12,7 @@ public class BungeeExtras extends JavaPlugin implements CommonExtras {
         BungeeExtras.plugin = this;
         this.getServer().getMessenger().registerOutgoingPluginChannel(this, "BungeeCord");
         this.getServer().getMessenger().registerIncomingPluginChannel(this, "BungeeCord", new PluginMessage());
+        Objects.requireNonNull(this.getCommand("inserv")).setExecutor(new ServerInfoCommand());
     }
 
     public static CommonExtras getInstance() {
